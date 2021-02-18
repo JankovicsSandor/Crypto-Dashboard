95aad819ccd175b6cc9840ecebc6446daed5117b
JankovicsSandor <sanyika717022@gmail.com>
2021-02-18 18:19:19 +0100

    Add subpages to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-{ path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule) }];
+{ path: 'setting', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule) },
+{ path: 'exchange', loadChildren: () => import('./pages/exchange/exchange.module').then(m => m.ExchangeModule) },
+{ path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
+{ path: 'history', loadChildren: () => import('./pages/history/history.module').then(m => m.HistoryModule) },
+{ path: 'support', loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule) },
+{ path: 'wallet', loadChildren: () => import('./pages/wallet/wallet.module').then(m => m.WalletModule) },
+{ path: 'notification', loadChildren: () => import('./pages/notification/notification.module').then(m => m.NotificationModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
