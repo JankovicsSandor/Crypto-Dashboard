f329eede7a065958418f4da2f779c8b96ccd6970
JankovicsSandor <sanyika717022@gmail.com>
2021-02-18 15:04:10 +0100

    Add toolbar to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseHttpClientService } from './base-client/base-http-client.service';
 import { StoreModule } from './store/store.module';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatIconModule, MatListModule, AppRoutingModule, MatExpansionModule, StoreModule, HttpClientModule, MatSidenavModule],
   providers: [BaseHttpClientService],
   bootstrap: [AppComponent],
 })
